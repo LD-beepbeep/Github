c89e4b150f2709ea31f20439417c56612c2470a0
Lars <lars.dehairs@gmail.com>
2026-02-27 18:03:10 +0100

    Create a dummy service worker for PWA
    Add a simple service worker for PWA support

---
+++ sw.js
@@ -0,0 +1,4 @@
+// A simple dummy service worker to satisfy PWA installation requirements
+self.addEventListener('fetch', (event) => {
+    // We don't need to do anything here for a simple redirect app
+});
